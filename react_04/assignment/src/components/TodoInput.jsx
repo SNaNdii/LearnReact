aa7b133915684c072330cd18f91d9b47a3484f53
SNaNdii <singhnandita73@gmail.com>
2024-02-20 19:16:54 +0530

    Complete ToDo list with UseEffect, added task in API

---
+++ react_04/assignment/src/components/TodoInput.jsx
@@ -0,0 +1,56 @@
+import {useEffect, useState } from "react"
+
+const TodoInput = () =>{
+    const[toDo, setTask] = useState([]);
+    const[text, settext] = useState("");
+
+    useEffect ( () => {
+        getData();
+    }, [])
+    
+    async function getData(){
+        try{
+            const res = await fetch("http://localhost:8080/list");
+            const data = await res.json();
+            setTask(data);
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    
+    return (
+        <div>
+            <input 
+                onChange={(e) => settext(e.target.value)}
+                type="text"
+                placeholder="Enter your task here" 
+            />
+
+            <button 
+                onClick={() => {
+                    const payload = {
+                        task : text,
+                        status : false,
+                    };
+                    fetch("http://localhost:8080/list", {
+                        mathod: "POST",
+                        headers : {
+                            "content-type" : "application/json",
+                        },
+                        body : JSON.stringify(payload),
+                    }).then (() => {
+                        getData();
+                    });
+                }} >Add
+            </button>
+            
+            {console.log(toDo)}
+            {toDo.map(({id, task}) => {
+                return <h1>Id is : {id} , Task is : {task} </h1>
+            })}
+        </div>
+    )
+}
+
+export default TodoInput
