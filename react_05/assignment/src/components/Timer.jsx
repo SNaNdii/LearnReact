ae635db7cdc6ec01ff15f3da36a40d102cf625a4
SNaNdii <singhnandita73@gmail.com>
2024-02-22 13:28:31 +0530

    Real Time Timer with days Complete

---
+++ react_05/assignment/src/components/Timer.jsx
@@ -0,0 +1,57 @@
+import {useState, useEffect} from 'react'
+
+function Timer(){
+    const[startsec ,setStartSec] = useState(0);
+    const[startMin ,setStartMin] = useState(0);
+    const[startHour ,setStartHour] = useState(0);
+    const[day ,setDay] = useState(0);
+
+    const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    useEffect( () => {
+        setInterval( () => {
+
+            setStartSec( (sec) => {
+
+                if(sec == 4){
+                    setStartMin((min) => {
+
+                        if(min == 4){
+                            setStartHour((hour) => {
+
+                                if(hour == 4 && min == 4 && sec == 4){
+                                    setDay( (d) => {
+
+                                        if(d == dayName.length){
+                                            return 0;
+                                        }
+                                        return d+1;
+
+                                    });
+                                    return 0;
+                                }
+
+                                return hour+1;
+                            })
+                            return 0;
+                        }
+
+                        return min+1;
+                    });
+                    return 0;
+                } 
+
+                return sec+1;
+            });
+            
+        }, 100);
+    }, [])
+
+    return (
+        <div>
+            <h4>{`Timer :- ${startHour} : ${startMin} : ${startsec}`}</h4>
+            <h3> Day : {dayName[day]}</h3>
+        </div>
+    )
+}
+
+export default Timer
