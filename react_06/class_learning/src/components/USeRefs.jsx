7191d19156c472d77ca4ab25d083ff4f7d35c07a
SNaNdii <singhnandita73@gmail.com>
2024-02-23 18:13:59 +0530

    UseRef Complete

--- react_06/class_learning/src/components/USeRefs.jsx
+++ react_06/class_learning/src/components/USeRefs.jsx
@@ -1,6 +1,48 @@
+import { useState, useRef, useEffect} from "react"
+
+/* ----------- UseRef -------------
+1. Store the value and dont+'t lose it when component is re-rendering
+2. Change in this value should not cause re-rendering
+ */
 
 function UseRefs(){
+    const[count, setCount] = useState(0);
+    const ref = useRef(10); // {current}
 
+    console.log("count is initially : ", ref)
+    useEffect( () => {
+        startCounter()
+    }, [])
+    
+    const startCounter = () => {
+        ref.current = setInterval( () => {
+            setCount( (c) => {
+                return c+1;
+            })
+        }, 500)
+    }
+    return (
+        <div>
+            <h2>Counter : {count} </h2>
+
+            <button onClick={() => {
+                setCount(count+1);
+            }} >Change Counter</button>
+            
+            <button onClick={() => {
+                ref.current += 1;  // It will store a reference in line no 10
+                console.log("Value changed : ", ref)
+            }} >Change Value</button>
+
+            <button onClick={() => {
+                clearInterval(ref.current);
+            }}>Pause Counter</button>
+
+            <button onClick={() => {
+                startCounter();
+            }}>Restart Counter</button>
+        </div>
+    )
 }
 
 export default UseRefs
