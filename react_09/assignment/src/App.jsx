50cfa760d30e5863bf9e16d13f1acc7679d5480e
SNaNdii <singhnandita73@gmail.com>
2024-03-01 23:52:29 +0530

    Redux app complete

--- react_09/assignment/src/App.jsx
+++ react_09/assignment/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Home } from './components/Home'
+import NavBar from "./components/Nav"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <NavBar/>
       <Home/>
     </div>
   )
