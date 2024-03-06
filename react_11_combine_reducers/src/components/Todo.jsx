1013b6b3e396aee0bfb5ec5ca4036b5801c9b11b
SNaNdii <singhnandita73@gmail.com>
2024-03-06 20:54:59 +0530

    Combine reducer complete

--- react_11_combine_reducers/src/components/Todo.jsx
+++ react_11_combine_reducers/src/components/Todo.jsx
@@ -14,11 +14,12 @@ export const Todo = () => {
                 status : false
             })
         );
+        setText("");
     }
 
     return (
         <div>
-            <input onChange={(e) => { setText(e.target.value) }} placeholder="Enter Task Here"/>
+            <input value={text} onChange={(e) => { setText(e.target.value) }} placeholder="Enter Task Here"/>
             <button onClick={todosHandleAdd}> Add Task</button>
             {todos.map((e, i) => {
                 return <li key={i}>{e.title}</li>
